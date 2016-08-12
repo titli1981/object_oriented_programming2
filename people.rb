e4ed6183b2a9b9aae79ebc417b6e4b50674c7634
titli1981 <madhumishra084@gmailcom>
2016-08-12 13:56:42 -0400

    Created person class and initialized

--- people.rb
+++ people.rb
@@ -1,4 +1,29 @@
-class Student    
+class Person
+  #attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+
+  def greeting
+    "Hi, my name is #{@name}"
+  end
 end
-class Instructor
+
+
+class Student < Person
+  def learn
+    "I get it"
+  end
 end
+
+
+class Instructor < Person
+  def teach
+    "Everything in Ruby is an Object"
+  end
+end
+
+chris = Instructor.new("Chris")
+puts chris.greeting
+cristina = Student.new("Cristina")
+puts cristina.greeting
